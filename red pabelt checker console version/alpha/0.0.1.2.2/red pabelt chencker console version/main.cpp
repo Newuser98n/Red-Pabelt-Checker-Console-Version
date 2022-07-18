024009541dd4e0dd0a711d57eb6eb3ca4024be09
Developer563 <109383284+Developer563@users.noreply.github.com>
2022-07-18 19:05:25 +0200

    error fix

---
+++ red pabelt checker console version/alpha/0.0.1.2.2/red pabelt chencker console version/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+
+
+
+int main()
+{
+
+float liczbaocen;
+
+
+
+float wsdcp = 4.75;
+
+int suma = 0;
+
+float srednia;
+
+
+count << "z ilu ocen chcesz poliszyć średnią?: " << endl;
+cin >> liczbaocen >> endl;
+
+float oceny [liczbaocen];
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ red pabelt checker console version/alpha/0.0.1.2.3/red pabelt chencker console version/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+
+
+
+int main()
+{
+
+int liczbaocen;
+
+
+
+float wsdcp = 4.75;
+
+int suma = 0;
+
+float srednia;
+
+
+cout << "z ilu ocen chcesz poliszyć średnią?: " << endl;
+cin >> liczbaocen;
+
+int oceny [liczbaocen];
+
+cout << "podaj 3 ocene?: " << endl;
+
+cin >> oceny[3];
+
+cout << oceny[3];
+
+
+
+
+
+
+
+
+
+
+}
